658a35d31c22822c1e2afb6190b61b8da91bd0b2
matsumoto-haxan <matsumoto@haxan.co.jp>
2019-09-30 16:48:18 +0900

    ダッシュボード画面のリンクなど

--- orihime/resources/views/home.blade.php
+++ orihime/resources/views/home.blade.php
@@ -3,18 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-10">
+            <div class="card cardmargin">
+                <div class="card-header">アラート</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    ※ここにアラート通知対象の発注情報が表示されます
+                </div>
+            </div>
 
-                    You are logged in!
+            <div class="card">
+                <div class="card-header">管理機能</div>
+
+                <div class="card-body">
+                    <button class="main_func_button" onclick="location.href='/order'">注文管理</button>
+                    <button class="main_func_button" onclick="location.href='/delivery'">出荷管理表</button>
+                    <button class="main_func_button" onclick="location.href='/shipping'">出荷依頼書</button>
+                    <button class="main_func_button" onclick="location.href='/admin'">マスタ編集</button>
                 </div>
             </div>
         </div>
